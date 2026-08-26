9a0b668f93249555579b1f6c794960f20a33d9a0
Deepthi <deepthi121416@gmail.com>
2026-08-26 13:01:41 +0530

    eks-terrafrom

--- 20-Bastion/main.tf
+++ 20-Bastion/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
     ami = local.ami_id
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.bastion.name
+    #iam_instance_profile = aws_iam_instance_profile.bastion.name
     instance_type = "t3.micro"
     root_block_device {
         volume_size = 50
